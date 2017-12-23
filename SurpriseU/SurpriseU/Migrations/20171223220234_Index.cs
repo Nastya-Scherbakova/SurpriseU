d7c73055f7ad75929830a14debfde0028cb0751e
Nastya Scherbakova <danskar@yandex.ua>
2017-12-24 00:50:19 +0200

    Model, context, controller and migrations added

---
+++ SurpriseU/SurpriseU/Migrations/20171223220234_Index.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SurpriseU.Migrations
+{
+    public partial class Index : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Presents",
+                columns: table => new
+                {
+                    PresentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<string>(maxLength: 100, nullable: false),
+                    Photo = table.Column<string>(maxLength: 100, nullable: false),
+                    Title = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Presents", x => x.PresentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Presents");
+        }
+    }
+}
---
+++ SurpriseU/SurpriseU/Models/Present.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurpriseU.Models
+{
+
+    public class Present
+    {
+        public int PresentId { get; set; }
+        [Required, StringLength(100)]
+        public string Title { get; set; }
+        [Required, StringLength(100)]
+        public string Content { get; set; }
+        [Required, StringLength(100)]
+        public string Photo { get; set; }
+
+
+    }
+    public class PresentsContext : DbContext
+    {
+        public PresentsContext(DbContextOptions<PresentsContext> options)
+           : base(options)
+        { }
+        public DbSet<Present> Presents { get; set; }
+
+    }
+}
