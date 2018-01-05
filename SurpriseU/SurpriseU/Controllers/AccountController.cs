dd05e9d90528138af3b1aaf47c24ac79e1243df6
Nastya Scherbakova <danskar@yandex.ua>
2018-01-06 01:44:34 +0200

    Role autherisation, adding admin

---
+++ SurpriseU/SurpriseU/Controllers/AccountController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using SurpriseU.ViewModels; // пространство имен моделей RegisterModel и LoginModel
+using SurpriseU.Models; // пространство имен UserContext и класса User
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace SurpriseU.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private ApplicationContext _context;
+        public AccountController(ApplicationContext context)
+        {
+            _context = context;
+            DatabaseInitialize(); // добавляем пользователя и роли в бд
+        }
+        [HttpGet]
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAdmin(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    user = new User { Email = model.Email, Password = model.Password };
+                    Role adminRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "admin");
+                    if (adminRole != null)
+                        user.Role = adminRole;
+
+                    _context.Users.Add(user);
+                    await _context.SaveChangesAsync();
+
+                   
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+        private void DatabaseInitialize()
+        {
+            if (!_context.Roles.Any())
+            {
+                string adminRoleName = "admin";
+                string userRoleName = "user";
+
+                string adminEmail = "dmitrievnaa@icloud.com";
+                string adminPassword = "n21111998";
+
+                string adminEmail2 = "ashmooree1@gmail.com";
+                string adminPassword2 = "12345678";
+
+                // добавляем роли
+                Role adminRole = new Role { Name = adminRoleName };
+                Role userRole = new Role { Name = userRoleName };
+
+
+                _context.Roles.Add(userRole);
+                _context.Roles.Add(adminRole);
+
+                // добавляем администратора
+                _context.Users.Add(new User { Email = adminEmail, Password = adminPassword, Role = adminRole });
+                _context.Users.Add(new User { Email = adminEmail2, Password = adminPassword2, Role = adminRole });
+
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    // добавляем пользователя в бд
+                    user = new User { Email = model.Email, Password = model.Password, Name=model.Name, Age=model.Age };
+                    Role userRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "user");
+                    if (userRole != null)
+                        user.Role = userRole;
+
+                    _context.Users.Add(user);
+                    await _context.SaveChangesAsync();
+
+                    await Authenticate(user); // аутентификация
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users
+                    .Include(u => u.Role)
+                    .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                if (user != null)
+                {
+                    await Authenticate(user); // аутентификация
+                    if (user.Role.Name == "admin") return RedirectToAction("AdminsCabinet", "Home");
+                    else return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+            return View(model);
+        }
+        private async Task Authenticate(User user)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+    }
+}
--- SurpriseU/SurpriseU/Controllers/UsersController.cs
+++ SurpriseU/SurpriseU/Controllers/UsersController.cs
@@ -9,117 +9,117 @@ using SurpriseU.Models;
 
 namespace SurpriseU.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Users")]
-    public class UsersController : Controller
-    {
-        private readonly ApplicationContext _context;
+    //[Produces("application/json")]
+    //[Route("api/Users")]
+    //public class UsersController : Controller
+    //{
+    //    private readonly ApplicationContext _context;
 
-        public UsersController(ApplicationContext context)
-        {
-            _context = context;
-        }
+    //    public UsersController(ApplicationContext context)
+    //    {
+    //        _context = context;
+    //    }
 
-        // GET: api/Users
-        [HttpGet]
-        public IEnumerable<User> GetUsers()
-        {
-            return _context.Users;
-        }
+    //    // GET: api/Users
+    //    [HttpGet]
+    //    public IEnumerable<User> GetUsers()
+    //    {
+    //        return _context.Users;
+    //    }
 
-        // GET: api/Users/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetUser([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+    //    // GET: api/Users/5
+    //    [HttpGet("{id}")]
+    //    public async Task<IActionResult> GetUser([FromRoute] int id)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
 
-            var user = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
+    //        var user = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+    //        if (user == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            return Ok(user);
-        }
+    //        return Ok(user);
+    //    }
 
-        // PUT: api/Users/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User user)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+    //    // PUT: api/Users/5
+    //    [HttpPut("{id}")]
+    //    public async Task<IActionResult> PutUser([FromRoute] int id, [FromBody] User user)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
 
-            if (id != user.Id)
-            {
-                return BadRequest();
-            }
+    //        if (id != user.Id)
+    //        {
+    //            return BadRequest();
+    //        }
 
-            _context.Entry(user).State = EntityState.Modified;
+    //        _context.Entry(user).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+    //        try
+    //        {
+    //            await _context.SaveChangesAsync();
+    //        }
+    //        catch (DbUpdateConcurrencyException)
+    //        {
+    //            if (!UserExists(id))
+    //            {
+    //                return NotFound();
+    //            }
+    //            else
+    //            {
+    //                throw;
+    //            }
+    //        }
 
-            return NoContent();
-        }
+    //        return NoContent();
+    //    }
 
-        // POST: api/Users
-        [HttpPost]
-        public async Task<IActionResult> PostUser([FromBody] User user)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+    //    // POST: api/Users
+    //    [HttpPost]
+    //    public async Task<IActionResult> PostUser([FromBody] User user)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+    //        _context.Users.Add(user);
+    //        await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUser", new { id = user.Id }, user);
-        }
+    //        return CreatedAtAction("GetUser", new { id = user.Id }, user);
+    //    }
 
-        // DELETE: api/Users/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+    //    // DELETE: api/Users/5
+    //    [HttpDelete("{id}")]
+    //    public async Task<IActionResult> DeleteUser([FromRoute] int id)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return BadRequest(ModelState);
+    //        }
 
-            var user = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+    //        var user = await _context.Users.SingleOrDefaultAsync(m => m.Id == id);
+    //        if (user == null)
+    //        {
+    //            return NotFound();
+    //        }
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+    //        _context.Users.Remove(user);
+    //        await _context.SaveChangesAsync();
 
-            return Ok(user);
-        }
+    //        return Ok(user);
+    //    }
 
-        private bool UserExists(int id)
-        {
-            return _context.Users.Any(e => e.Id == id);
-        }
-    }
+    //    private bool UserExists(int id)
+    //    {
+    //        return _context.Users.Any(e => e.Id == id);
+    //    }
+    //}
 }
