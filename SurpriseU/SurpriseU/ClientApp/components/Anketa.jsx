8053315f04551bea7942ece689f8a2f58c177780
Vlada Doroshenko <ashmooree1@gmail.com>
2018-01-17 21:00:54 +0200

    Encoding

--- SurpriseU/SurpriseU/ClientApp/components/Anketa.jsx
+++ SurpriseU/SurpriseU/ClientApp/components/Anketa.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import '../css/style.css';
 import '../css/bootstrap.css';
 import { PresentsList, Present } from './Present.jsx';
--- SurpriseU/SurpriseU/ClientApp/components/Present.jsx
+++ SurpriseU/SurpriseU/ClientApp/components/Present.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import '../css/style.css';
 import { Link, NavLink } from 'react-router-dom';
 
