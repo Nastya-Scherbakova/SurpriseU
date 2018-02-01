476ff0b8a175c99941a7ad9d2133b685b84b098c
Nastya Scherbakova <danskar@yandex.ua>
2018-02-01 23:16:04 +0200

    upd with public strings

---
+++ SurpriseU/SurpriseU/Migrations/Presents/20180201211413_PrCorrectPublic.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SurpriseU.Migrations.Presents
+{
+    public partial class PrCorrectPublic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CelebrationString",
+                table: "Presents",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LikesString",
+                table: "Presents",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsersIdString",
+                table: "Presents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CelebrationString",
+                table: "Presents");
+
+            migrationBuilder.DropColumn(
+                name: "LikesString",
+                table: "Presents");
+
+            migrationBuilder.DropColumn(
+                name: "UsersIdString",
+                table: "Presents");
+        }
+    }
+}
