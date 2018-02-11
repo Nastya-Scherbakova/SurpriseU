03f8313a1019b54736768033426a6a4a5f39f7ef
ghbdtnkjh98 <ghbdtnkjh98@gmail.com>
2018-02-11 20:07:41 +0200

    not ready seed

--- SurpriseU/SurpriseU/Initializers/RoleInitializer.cs
+++ SurpriseU/SurpriseU/Initializers/RoleInitializer.cs
@@ -33,8 +33,261 @@ namespace SurpriseU.Initializers
                 {
                     await userManager.AddToRoleAsync(admin, "admin");
                     await userManager.AddToRoleAsync(admin2, "admin");
-                    
+
                 }
+            }
+        }
+    }
+
+    public class PresentsTagInitializer
+    {
+
+        public static void Initialize(ApplicationContext context)
+        {
+            bool presentTag = false;
+            if (!context.Presents.Any())
+            {
+                presentTag = true;
+                var presents = new List<Present>
+                {
+                    new Present
+                    {
+                        Id = "1",
+                        Title = "Кекс",
+                        Content = "Смачний подарунок для твоєї пишечки",
+                        Gender = PresentsGender.All,
+                        Photo  = "http://qoo.by/3PET",
+                        StartAge = 10,
+                        EndAge  = 85
+                        //List<PresentTag> Tags
+                        //List<UserPresent> Users//users that liked this present
+                        //User User
+                        //List<PresentUser> likedForUsers
+                    },
+                    new Present
+                    {
+                        Id = "2",
+                        Title = "Торт",
+                        Content = "Смачний подарунок до вашого святкового столу",
+                        Gender = PresentsGender.All,
+                        Photo = "http://qoo.by/3PES",
+                        StartAge = 5,
+                        EndAge = 85,
+                        //List<PresentTag> Tags
+                        //List<UserPresent> Users//users that liked this present
+                        //User User
+                        //List<PresentUser> likedForUsers
+                    },
+                    new Present
+                    {
+                        Id = "3",
+                        Title = "PowerBank",
+                        Content = "Залишайся на зв'язку навіть під час подорожі",
+                        Gender = PresentsGender.All,
+                        Photo = "http://qoo.by/3PEQ",
+                        StartAge = 14,
+                        EndAge = 85,
+                        //List<PresentTag> Tags
+                        //List<UserPresent> Users//users that liked this present
+                        //User User
+                        //List<PresentUser> likedForUsers
+                    },
+                     new Present
+                     {
+                         Id = "4",
+                         Title = "Кульки",
+                         Content = "Подаруй легкість та яскравість",
+                         Gender = PresentsGender.Female,
+                         Photo = "http://qoo.by/3PEP",
+                         StartAge = 10,
+                         EndAge = 85,
+                         //List<PresentTag> Tags
+                         //List<UserPresent> Users//users that liked this present
+                         //User User
+                         //List<PresentUser> likedForUsers
+                     },
+                      new Present
+                      {
+                          Id = "5",
+                          Title = "Святковий набір",
+                          Content = "Чудовий вибір, коли бажаєш підкреслити різносторонність людини",
+                          Gender = PresentsGender.All,
+                          Photo = "http://qoo.by/3PF4",
+                          StartAge = 16,
+                          EndAge = 85,
+                          //List<PresentTag> Tags
+                          //List<UserPresent> Users//users that liked this present
+                          //User User
+                          //List<PresentUser> likedForUsers
+                      },
+                       new Present
+                       {
+                           Id = "6",
+                           Title = "Навушники",
+                           Content = "Накращий подарунок для твого геймера",
+                           Gender = PresentsGender.Male,
+                           Photo = "http://qoo.by/3PF6",
+                           StartAge = 10,
+                           EndAge = 40,
+                           //List<PresentTag> Tags
+                           //List<UserPresent> Users//users that liked this present
+                           //User User
+                           //List<PresentUser> likedForUsers
+                       },
+                        new Present
+                        {
+                            Id = "7",
+                            Title = "XBox 360",
+                            Content = "Смачний подарунок для твоєї пишечки",
+                            Gender = PresentsGender.Male,
+                            Photo = "http://qoo.by/3PFp",
+                            StartAge = 10,
+                            EndAge = 85,
+                            //List<PresentTag> Tags
+                            //List<UserPresent> Users//users that liked this present
+                            //User User
+                            //List<PresentUser> likedForUsers
+                        },
+                         new Present
+                         {
+                             Id = "8",
+                             Title = "L.O.L.",
+                             Content = "Подарунок для маленької красуні",
+                             Gender = PresentsGender.Female,
+                             Photo = "http://qoo.by/3PFq",
+                             StartAge = 3,
+                             EndAge = 11,
+                             //List<PresentTag> Tags
+                             //List<UserPresent> Users//users that liked this present
+                             //User User
+                             //List<PresentUser> likedForUsers
+                         },
+                          new Present
+                          {
+                              Id = "9",
+                              Title = "LunchBox",
+                              Content = "Завжди ситий - завжди щасливий",
+                              Gender = PresentsGender.All,
+                              Photo = "http://qoo.by/3PFA",
+                              StartAge = 10,
+                              EndAge = 85,
+                              //List<PresentTag> Tags
+                              //List<UserPresent> Users//users that liked this present
+                              //User User
+                              //List<PresentUser> likedForUsers
+                          },
+                          new Present
+                          {
+                              Id = "10",
+                              Title = "Настільна гра",
+                              Content = "Дозволить Вам частіше збиратись компанією",
+                              Gender = PresentsGender.All,
+                              Photo = "http://qoo.by/3PFH",
+                              StartAge = 8,
+                              EndAge = 85,
+                              //List<PresentTag> Tags
+                              //List<UserPresent> Users//users that liked this present
+                              //User User
+                              //List<PresentUser> likedForUsers
+                          },
+                          new Present
+                          {
+                              Id = "11",
+                              Title = "Щоденник Insta-girl",
+                              Content = "Допоможе тобі організувати свій день та Instagram",
+                              Gender = PresentsGender.Female,
+                              Photo = "http://qoo.by/3PFA",
+                              StartAge = 8,
+                              EndAge = 50,
+                              //List<PresentTag> Tags
+                              //List<UserPresent> Users//users that liked this present
+                              //User User
+                              //List<PresentUser> likedForUsers
+                          }
+                };
+                
+                
+
+
+
+            }
+            if (!context.Tags.Any())
+            {
+                presentTag = true;
+                var tags = new List<Tag>
+                {
+                    new Tag
+                    {
+                        Id = "1",
+                        Name = "Куховарство",
+                        Type = Models.Type.Likes
+                    },
+                     new Tag
+                    {
+                        Id = "2",
+                        Name = "Instagram",
+                        Type = Models.Type.Likes
+                    },
+                      new Tag
+                    {
+                        Id = "3",
+                        Name = "Gaming",
+                        Type = Models.Type.Likes
+                    },
+                       new Tag
+                    {
+                        Id = "4",
+                        Name = "Ґаджети",
+                        Type = Models.Type.Likes
+                    },
+                        new Tag
+                    {
+                        Id = "5",
+                        Name = "Солодке",
+                        Type = Models.Type.Likes
+                    },
+                        new Tag
+                    {
+                        Id = "6",
+                        Name = "День Народження",
+                        Type = Models.Type.Celebration
+                    },
+                        new Tag
+                    {
+                        Id = "7",
+                        Name = "Новий Рік",
+                        Type = Models.Type.Celebration
+                    },
+                        new Tag
+                    {
+                        Id = "8",
+                        Name = "День закоханих",
+                        Type = Models.Type.Celebration
+                    },
+                        new Tag
+                    {
+                        Id = "9",
+                        Name = "Жіночий день",
+                        Type = Models.Type.Celebration
+                    },
+                        new Tag
+                    {
+                        Id = "10",
+                        Name = "Чоловічий день",
+                        Type = Models.Type.Celebration
+                    }
+                };
+                tags.ForEach(s => context.AddRange(s));
+
+            }
+            if (presentTag)
+            {
+                context.AddRange(
+                    new PresentTag
+                    {
+                        
+                    }
+                   );
             }
         }
     }
